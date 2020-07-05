5567c6579dcc5f4eba7e1588069ca4e48c7f4357
daqi <daqi@bytedance.com>
2020-07-05 17:30:49 +0800

    show define a method in script

--- android-basics/app/src/main/java/android/basics/MainActivity.java
+++ android-basics/app/src/main/java/android/basics/MainActivity.java
@@ -3,6 +3,7 @@ package android.basics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("daqi", "current branch HEAD: " + BuildConfig.commitHash);
     }
 }
